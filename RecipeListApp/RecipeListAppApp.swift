cf6191ad28fb6b89417f98a7fc6ea6cb53a5dd15
Jacob Scase <jacobscase@hotmail.com>
2021-06-13 10:48:47 +0100

    Initial Commit

---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by Jacob Scase on 13/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
