3ce242a75008622453f3f5fa54134bbddefa022d
Jacob Scase <jacobscase@hotmail.com>
2021-06-20 15:57:42 +0100

    Update detail view for more consistent styling

--- RecipeListApp/Views/RecipeListView.swift
+++ RecipeListApp/Views/RecipeListView.swift
@@ -14,27 +14,41 @@ struct RecipeListView: View {
     
     var body: some View {
         NavigationView {
-            List(rModel.recipes){ r in
-                
-                NavigationLink(
-                    destination: RecipeDetailView(recipe: r),
-                    label: {
-                        HStack(spacing: 20.0){
-                            Image(r.image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50
-                                       , height: 50, alignment: .center)
-                                .clipped()
-                                .cornerRadius(30.0)
-                            Text(r.name)
+            
+            VStack(alignment: .leading) {
+                Text("All Recipes")
+                    .font(.largeTitle)
+                    .bold()
+                    
+                    .padding(.top,40)
+                ScrollView {
+                    LazyVStack(alignment: .leading){
+                        ForEach(rModel.recipes){ r in
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe: r),
+                                label: {
+                                    HStack(spacing: 20.0){
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50
+                                                   , height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(30.0)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                    }
+                                })
+                            
+                            
+                            
                         }
-                    })
-                
-                
-                
+                        .navigationBarHidden(true)
+                    }
+                }
             }
-            .navigationBarTitle("All Recipes")
+            //            .navigationBarTitle("All Recipes")
+            .padding(.leading)
         }
     }
 }
@@ -42,5 +56,6 @@ struct RecipeListView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
